173a27189afbcc0dc95b1dd5a629838a3f3261bb
Edna <50573134+ednamafukashe@users.noreply.github.com>
2024-08-07 14:45:33 +0200

    Added search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
+function refreshWeather(response){
+    let temperatureElement=document.querySelector("#temperature")
+    let temperature= response.data.temperature.current
+    let cityElement =document.querySelector("#city")
+    cityElement.innerHTML = response.data.city
 
+    temperatureElement.innerHTML= Math.round(temperature)
+   
+    
+
+}
+
+function searchCity(city){
+    let apiKey="982o4f7ffabt7303d3885b043bf71d70"
+    apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`
+    console.log(apiUrl)
+
+axios.get(apiUrl).then(refreshWeather)
+}
 function handleSearch(event){
     event.preventDefault();
     let searchInputElement = document.querySelector("#searchInput")
-    let cityElement =document.querySelector("#city")
-    cityElement.innerHTML = searchInputElement.value
+    
+
+    searchCity(searchInputElement.value)
 
 
 }
 
 let searchForm = document.querySelector("#search") 
 searchForm.addEventListener("submit",handleSearch)
+searchCity("Harare")
